4ffc4553bd1c327912b6253e2f408a211da62ae3
Akarsh <akarsh.1995.02@gmail.com>
2019-10-20 20:28:52 +0530

    changed environ pick

--- portfolio_site/settings/prod.py
+++ portfolio_site/settings/prod.py
@@ -5,11 +5,11 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_env_var('DATABASE_NAME'),
-        'USER': get_env_var('DATABASE_USER'),
-        'PASSWORD': get_env_var('DATABASE_PASSWORD'),
-        'HOST': get_env_var('DATABASE_HOST'),
-        'PORT': get_env_var('DATABASE_PORT'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
