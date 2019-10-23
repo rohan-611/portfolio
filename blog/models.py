d10236f0949a2e1558a1a995ea68f61bc1f0678e
rohan-611 <rohanraghuvanshi001@gmail.com>
2019-10-23 09:38:32 +0530

    Added blank and null to True.

--- blog/models.py
+++ blog/models.py
@@ -3,50 +3,50 @@ from django.db import models
 # Create your models here.
 
 class Categories(models.Model):
-    category = models.CharField(max_length = 50)
+    category = models.CharField(max_length = 50, blank=True, null=True)
 
     def __str__(self):
         return self.category
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=256)
-    picture = models.ImageField(upload_to='blog_pictures', default='default.jpg')
+    title = models.CharField(max_length=256, blank=True, null=True)
+    picture = models.ImageField(upload_to='blog_pictures', default='default.jpg', blank=True, null=True)
     category = models.ForeignKey(Categories, blank=True, null=True, on_delete=models.CASCADE)
-    content = models.TextField()
-    author = models.CharField(max_length=50)
-    date = models.DateField()
-    draft = models.BooleanField(default=0)
+    content = models.TextField(blank=True, null=True)
+    author = models.CharField(max_length=50, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    draft = models.BooleanField(default=0, blank=True, null=True)
 
     def __str__(self):
         return self.title
 
 class Teachings(models.Model):
 
-    title = models.CharField( max_length=50)
-    content = models.TextField()
-    display = models.BooleanField(default=True)
+    title = models.CharField( max_length=50, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    display = models.BooleanField(default=True, blank=True, null=True)
 
     def __str__(self):
         return self.content
 
 class Comments(models.Model):
-    blog = models.ForeignKey(Blog , on_delete=models.CASCADE)
-    user = models.CharField(max_length=50)
-    comment = models.TextField()
-    date = models.DateField(auto_now=False)
-    time = models.TimeField(auto_now=False)
+    blog = models.ForeignKey(Blog , on_delete=models.CASCADE, blank=True, null=True)
+    user = models.CharField(max_length=50, blank=True, null=True)
+    comment = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now=False, blank=True, null=True)
+    time = models.TimeField(auto_now=False, blank=True, null=True)
 
     def __str__(self):
         return self.comment
 
 
 class Replies(models.Model):
-    comment_id = models.ForeignKey(Comments, on_delete=models.CASCADE)
-    user = models.TextField()
-    comment = models.TextField()
-    date = models.DateField(auto_now=True)
-    time = models.TimeField(auto_now=True)
+    comment_id = models.ForeignKey(Comments, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.TextField(blank=True, null=True)
+    comment = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now=True, blank=True, null=True)
+    time = models.TimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return self.comment
--- portfolio/models.py
+++ portfolio/models.py
@@ -4,50 +4,50 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
-    designation = models.CharField(max_length=100)
-    bachelors = models.CharField(max_length=150)
-    masters = models.CharField(max_length=150)
-    phd = models.CharField(max_length=150)
-    bio = models.TextField()
+    name = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    designation = models.CharField(max_length=100, blank=True, null=True)
+    bachelors = models.CharField(max_length=150, blank=True, null=True)
+    masters = models.CharField(max_length=150, blank=True, null=True)
+    phd = models.CharField(max_length=150, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
     
 class About(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+    title = models.CharField(max_length=50, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
     
 
 class Categories(models.Model):
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.category
 
 class Portfolio(models.Model):
-    title = models.CharField(max_length=150)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='project_images', default='default.png')
-    desc = models.TextField()
-    details = models.TextField()
-    date = models.DateField(auto_now=False)
-    display = models.BooleanField(default = 0)
+    title = models.CharField(max_length=150, blank=True, null=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, blank=True, null=True)
+    picture = models.ImageField(upload_to='project_images', default='default.png', blank=True, null=True)
+    desc = models.TextField(blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now=False, blank=True, null=True)
+    display = models.BooleanField(default = 0, blank=True, null=True)
 
     def __str__(self):
         return self.title
     
 
 class ResearchAndPublications(models.Model):
-    title = models.CharField(max_length=150)
-    desc = models.TextField()
-    details = models.TextField()
-    date = models.DateField()
-    display = models.BooleanField(default = 0)
+    title = models.CharField(max_length=150, blank=True, null=True)
+    desc = models.TextField(blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    display = models.BooleanField(default = 0, blank=True, null=True)
 
     def __str__(self):
         return self.title
