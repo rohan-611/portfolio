162502264aa3e250569bf6419b5541067142ea14
Akarsh Jain <akarsh.1995.02@gmail.com>
2020-01-30 20:07:51 +0530

    changes

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-import dotenv
-dotenv.load_dotenv()
+
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE'))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          os.environ.get('DJANGO_SETTINGS_MODULE'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
